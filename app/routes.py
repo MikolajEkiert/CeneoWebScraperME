a67dc38cd0ae8ef8601cd2f1697efe093791d999
s-121-14 <s-121-14@gumiforest.ae.krakow.pl>
2024-05-27 14:47:44 +0200

    2024-05-27 ME (needs fix)

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 from . import utils
 import json
 import os
+import pandas as pd
+import numpy as np
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -19,6 +21,7 @@ def extract():
             page_dom = BeautifulSoup(response.text, "html.parser")
             opinions_count = utils.extract(page_dom, "a.product-review__link > span")
             if opinions_count:
+                product_name = 
                 all_opinions = []
                 while(url):
                     response = requests.get(url)
@@ -40,6 +43,20 @@ def extract():
                         os.makedirs("opinions")
                     with open(f"opinions/{product_id}.json", "w", encoding="UTF-8") as jf:
                         json.dump(all_opinions,jf ,  indent=4, ensure_ascii=False)
+                    opinions = pd.DataFrame.from_dict(all_opinions)
+                    opinions = pd.read_json(f"opinions/{product_id}.json")
+                    opinions.stars = opinions.stars.apply(lambda s: s.split('/')[0].replace(',',".")).astype(float)
+                    opinions.recommendation = opinions.recommendation.apply(lambda r: "Brak rekomendacji" if r is None else r )
+                    stats= {
+                        'product_id' : product_id,
+                        'product_name' : product_name ,
+                        'opinions_count' :  opinions.shape[0],
+                        'pros_count' : int(opinions.pros.apply(lambda p: None if p else p).count()),
+                        'cons_count ' :  int(opinions.cons.apply(lambda c: None if c else c).count()),
+                        'average_stars' : opinions.stars.mean(),
+                        'stars_distribution' : opinions.stars.value_counts().reindex(list(np.arange(0,5.5,0.5)),fill_value = 0).to_dict(),
+                        'recommendation' : opinions.recommendation.value_counts(dropna=False).reindex(["Polecam", "Brak rekomendacji", "Nie polecam"], fill_value = 0).to_dict()
+                    }
                 return redirect(url_for('product', product_id=product_id))
             return render_template("extract.html", error="Podany produkt nie ma opinii")
         return render_template("extract.html", error="Podany produkt nie istnieje")
@@ -49,12 +66,23 @@ def extract():
     
 @app.route('/products')
 def products():
-    return render_template("products.html")
+    products_list = [filename.split(".")[0] for filename in os.listdir("app/opinions")]
+    products = []
+    for product_id in products_list:
+        with open(f"app/products/{product_id}.json", "r", encoding="UTF-8") as jf:
+            products.append(json.load(jf))
+    return render_template("products.html", products=products)
 
 @app.route('/about')
 def about():
     return render_template("about.html")
 
-@app.route('/product/<product_id>')
-def product(product_id):
-    return render_template("product.html", product_id=product_id)
+@app.route('/product/download_json/<product_id>')
+def download_json(product_id):
+    return send_file(f"opinions/{product_id}.json","text/json", as_attachment=True)
+
+@app.route('/product/download_csv/<product_id>')
+def download_csv(product_id):
+    opinions = pd.read_json(f"opinions/")
+    buffer = io.BytesIO(opinions.to_csv().encode())
+    return send_file(buffer, "text/csv", as_attachment=True, download_name(f"<product_id>.csv"))
